adf04e4bb56db611cf9a51e58dffcad42e54b91e
d4nze <75412495+d4nze@users.noreply.github.com>
2025-07-18 04:00:31 +0300

    Created EditorPaletteWindow.

---
+++ BlueprintEditor/Include/Blueprint/Editor/EditorPaletteWindow.hpp
@@ -0,0 +1,27 @@
+#ifndef BLUEPRINT_EDITOR_EDITOR_PALETTE_WINDOW_HPP
+#define BLUEPRINT_EDITOR_EDITOR_PALETTE_WINDOW_HPP
+
+#include "Blueprint/Editor/EditorPalette.hpp"
+#include "Blueprint/Editor/Window.hpp"
+
+namespace Blueprint::Editor {
+class EditorPaletteWindow final : public Window {
+public:
+    explicit EditorPaletteWindow(WindowManager& manager);
+
+public:
+    void setPalette(EditorPalette* palette);
+
+private:
+    void gui() override;
+    void guiEmpty();
+    void guiItem(const EditorPalette::Item& item);
+
+private:
+    EditorPalette* m_Palette;
+    const float m_IconSize;
+    const float m_Space;
+};
+} // Blueprint::Editor
+
+#endif // BLUEPRINT_EDITOR_EDITOR_PALETTE_WINDOW_HPP
---
+++ BlueprintEditor/Source/Blueprint/Editor/EditorPaletteWindow.cpp
@@ -0,0 +1,99 @@
+#include "Blueprint/Editor/EditorPaletteWindow.hpp"
+
+#include <algorithm>
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <SFML/Graphics/Sprite.hpp>
+
+Blueprint::Editor::EditorPaletteWindow::EditorPaletteWindow(WindowManager& manager)
+: Window(manager, "Editor Palette", true)
+, m_Palette(nullptr)
+, m_IconSize(60.f)
+, m_Space(10.f) {}
+
+void Blueprint::Editor::EditorPaletteWindow::setPalette(EditorPalette* palette) {
+    m_Palette = palette;
+}
+
+void Blueprint::Editor::EditorPaletteWindow::gui() {
+    if (m_Palette == nullptr || m_Palette->getItemCount() == 0) {
+        return guiEmpty();
+    }
+    const ImVec2 padding = ImGui::GetContentRegionAvail();
+    const int columnsCount = std::max<int>(padding.x / (m_IconSize + m_Space * 2.f), 1);
+    if (!ImGui::BeginTable("Editor Item Table", columnsCount, ImGuiTableFlags_Borders)) {
+        return;
+    }
+    for (std::size_t i = 0; i < m_Palette->getItemCount(); i++) {
+        if (i % columnsCount == 0) {
+            ImGui::TableNextRow();
+        }
+        ImGui::TableNextColumn();
+        guiItem(m_Palette->getItem(i));
+    }
+    ImGui::EndTable();
+}
+
+void Blueprint::Editor::EditorPaletteWindow::guiEmpty() {
+    const char* text = "Palette is empty";
+    const ImVec2 textSize = ImGui::CalcTextSize(text);
+    const ImVec2 padding = ImGui::GetContentRegionAvail();
+    const ImVec2 position = {(padding.x - textSize.x) * 0.5f, (padding.y - textSize.y) * 0.5f};
+    ImGui::SetCursorPos(position);
+    ImGui::Text(text);
+}
+
+void Blueprint::Editor::EditorPaletteWindow::guiItem(const EditorPalette::Item& item) {
+    ImGui::BeginGroup();
+    ImGui::PushID(&item);
+    ImGuiStyle& style = ImGui::GetStyle();
+    const float originalCellPadding = style.CellPadding.x;
+    const float originalFramePadding = style.FramePadding.x;
+    style.CellPadding.x = 0.f;
+    style.FramePadding.x = 0.f;
+
+    const float columnWidth = ImGui::GetColumnWidth();
+    const float buttonOffset = (columnWidth - m_IconSize) * 0.5f;
+    ImGui::SetCursorPosX(ImGui::GetCursorPosX() + buttonOffset);
+    ImGui::SetCursorPosY(ImGui::GetCursorPosY() + m_Space / 2.f);
+
+    const bool isSelectedItem = m_Palette->getSelectedItem().name == item.name;
+    if (isSelectedItem) {
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.26f, 0.59f, 0.98f, 0.40f));
+        ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0.26f, 0.59f, 0.98f, 1.00f));
+        ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 2.0f);
+    }
+    bool isButtonPressed = false;
+    if (item.texture == nullptr) {
+        isButtonPressed = ImGui::Button("##Item Button", ImVec2(m_IconSize, m_IconSize));
+    } else {
+        const sf::Sprite sprite(*item.texture);
+        isButtonPressed = ImGui::ImageButton("##Item Button", sprite, sf::Vector2f(m_IconSize, m_IconSize));
+    }
+    if (isSelectedItem) {
+        ImGui::PopStyleVar();
+        ImGui::PopStyleColor(2);
+    }
+    if (isButtonPressed) {
+        m_Palette->selectItem(item);
+    }
+
+    const float textWidth = ImGui::CalcTextSize(item.name).x;
+    const float textOffset = (columnWidth - textWidth) * 0.5f;
+    ImGui::SetCursorPosX(ImGui::GetCursorPosX() + textOffset);
+    if (textWidth <= columnWidth) {
+        ImGui::Text(item.name);
+    } else {
+        ImGui::Text("...");
+        if (ImGui::IsItemHovered()) {
+            ImGui::BeginTooltip();
+            ImGui::TextDisabled(item.name);
+            ImGui::EndTooltip();
+        }
+    }
+
+    style.CellPadding.x = originalCellPadding;
+    style.FramePadding.x = originalFramePadding;
+    ImGui::PopID();
+    ImGui::EndGroup();
+}
