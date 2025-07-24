f313fb0077aecdfc5a7497c7fd7cb635a4683827
d4nze <75412495+cartavii@users.noreply.github.com>
2025-07-24 11:00:58 +0300

    Created SceneManager.

---
+++ BlueprintEditor/Include/Blueprint/Editor/SceneManager.hpp
@@ -0,0 +1,72 @@
+#ifndef BLUEPRINT_EDITOR_SCENE_MANAGER_HPP
+#define BLUEPRINT_EDITOR_SCENE_MANAGER_HPP
+
+#include "Blueprint/Core/Exception.hpp"
+#include "Blueprint/Resources/ResourceManager.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace Blueprint::Editor {
+class Application;
+class SceneEditor;
+class SceneFabric;
+
+class SceneTypeMissing final : public Core::Exception {
+public:
+    explicit SceneTypeMissing(const std::filesystem::path& path);
+};
+
+class SceneAlreadyLoaded final : public Core::Exception {
+public:
+    explicit SceneAlreadyLoaded(const std::filesystem::path& path);
+};
+
+class SceneIsNotLoaded final : public Core::Exception {
+public:
+    explicit SceneIsNotLoaded(const std::filesystem::path& path);
+};
+
+class SceneManager : private Resources::ResourceManager {
+public:
+    struct SceneHolder {
+        SceneEditor* sceneEditor;
+        std::string keyName;
+        std::filesystem::path path;
+    };
+    using iterator = std::vector<SceneHolder>::iterator;
+    using constIterator = std::vector<SceneHolder>::const_iterator;
+
+public:
+    explicit SceneManager(Application& application);
+    ~SceneManager();
+
+public:
+    void loadSceneEditor(const std::filesystem::path& path);
+    void unloadSceneEditor(const std::filesystem::path& path);
+    void clear();
+
+    [[nodiscard]] Application& getApplication();
+    [[nodiscard]] const Application& getApplication() const;
+
+    [[nodiscard]] std::size_t getSceneHolderCount() const;
+
+    iterator begin();
+    iterator end();
+    constIterator begin() const;
+    constIterator end() const;
+
+private:
+    nlohmann::json loadSceneData(const std::filesystem::path& path);
+
+private:
+    Application& m_Application;
+    SceneFabric& m_SceneFabric;
+    std::vector<SceneHolder> m_SceneHolders;
+};
+} // Blueprint::Editor
+
+#endif // BLUEPRINT_EDITOR_SCENE_MANAGER_HPP
---
+++ BlueprintEditor/Source/Blueprint/Editor/SceneManager.cpp
@@ -0,0 +1,101 @@
+#include "Blueprint/Editor/SceneManager.hpp"
+#include "Blueprint/Editor/Application.hpp"
+#include "Blueprint/Editor/SceneEditor.hpp"
+
+#include <fstream>
+
+Blueprint::Editor::SceneTypeMissing::SceneTypeMissing(const std::filesystem::path& path)
+: Exception("Missing 'Type' field inside file '" + path.string() + "'") {}
+
+Blueprint::Editor::SceneAlreadyLoaded::SceneAlreadyLoaded(const std::filesystem::path& path)
+: Exception("Scene with path '" + path.string() + "' is already loaded") {}
+
+Blueprint::Editor::SceneIsNotLoaded::SceneIsNotLoaded(const std::filesystem::path& path)
+: Exception("Failed to find scene with path '" + path.string() + "'") {}
+
+Blueprint::Editor::SceneManager::SceneManager(Application& application)
+: ResourceManager("Scenes")
+, m_Application(application)
+, m_SceneFabric(application.getSceneFabric()) {}
+
+Blueprint::Editor::SceneManager::~SceneManager() {
+    clear();
+}
+
+void Blueprint::Editor::SceneManager::loadSceneEditor(const std::filesystem::path& path) {
+    for (const auto [sceneEditor, keyName, otherPath] : m_SceneHolders) {
+        if (otherPath == path) {
+            throw SceneAlreadyLoaded(path);
+        }
+    }
+    if (!isPathValid(path)) {
+        throw Resources::FailedToOpenFileException(path.string());
+    }
+    nlohmann::json sceneData = loadSceneData(path);
+    if (!sceneData.contains("Type")) {
+        throw SceneTypeMissing(path);
+    }
+    const std::string sceneType = sceneData["Type"].get<std::string>();
+    SceneEditor* sceneEditor = m_SceneFabric.createSceneEditor(sceneType);
+    sceneEditor->load(sceneData);
+    m_SceneHolders.push_back({sceneEditor, sceneType, path});
+}
+
+void Blueprint::Editor::SceneManager::unloadSceneEditor(const std::filesystem::path& path) {
+    for (auto it = begin(); it != end(); ++it) {
+        if (auto [sceneEditor, keyName, otherPath] = *it; otherPath == path) {
+            nlohmann::json sceneData = loadSceneData(path);
+            if (!sceneData.contains("Type")) {
+                sceneData["Type"] = keyName;
+            }
+            sceneEditor->save(sceneData);
+            delete sceneEditor;
+            m_SceneHolders.erase(it);
+            return;
+        }
+    }
+}
+
+void Blueprint::Editor::SceneManager::clear() {
+    for (const auto [sceneEditor, keyName, path] : m_SceneHolders) {
+        delete sceneEditor;
+    }
+}
+
+Blueprint::Editor::Application& Blueprint::Editor::SceneManager::getApplication() {
+    return m_Application;
+}
+
+const Blueprint::Editor::Application& Blueprint::Editor::SceneManager::getApplication() const {
+    return m_Application;
+}
+
+std::size_t Blueprint::Editor::SceneManager::getSceneHolderCount() const {
+    return m_SceneHolders.size();
+}
+
+Blueprint::Editor::SceneManager::iterator Blueprint::Editor::SceneManager::begin() {
+    return m_SceneHolders.begin();
+}
+
+Blueprint::Editor::SceneManager::iterator Blueprint::Editor::SceneManager::end() {
+    return m_SceneHolders.end();
+}
+
+Blueprint::Editor::SceneManager::constIterator Blueprint::Editor::SceneManager::begin() const {
+    return m_SceneHolders.begin();
+}
+
+Blueprint::Editor::SceneManager::constIterator Blueprint::Editor::SceneManager::end() const {
+    return m_SceneHolders.end();
+}
+
+nlohmann::json Blueprint::Editor::SceneManager::loadSceneData(const std::filesystem::path& path) {
+    std::ifstream file(path);
+    nlohmann::json sceneData;
+    if (file.is_open()) {
+        file >> sceneData;
+        file.close();
+    }
+    return sceneData;
+}
