15da7c4077cb6f76a2df4f1ed0b083fc71d2935a
cartavii <75412495+cartavii@users.noreply.github.com>
2025-07-26 04:25:40 +0300

    Added Example::MyScene in register.

--- Editor/Source/Example/Application.cpp
+++ Editor/Source/Example/Application.cpp
@@ -1,8 +1,11 @@
 #include "Example/Application.hpp"
+#include "Example/MyScene.hpp"
 
 Example::Application::Application()
 : Blueprint::Editor::Application("Editor Demo")
 , m_EditorPalette({}) {
+    Blueprint::Editor::SceneFabric& sceneFabric = getSceneFabric();
+    sceneFabric.registerSceneEditor<MyScene>("Example::MyScene");
     Blueprint::Editor::PaletteWindow& paletteWindow = getWindowManager().getEditorPaletteWindow();
     Blueprint::Resources::TextureManager& textureManager = getTextureManager();
     m_EditorPalette = {{textureManager.getTextureResource("Icon 1.png"), "Icon 1"},
