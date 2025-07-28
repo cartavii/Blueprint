ad2e721d0b20a3f7439f3c297f0d024d1e371b4c
d4nze <75412495+cartavii@users.noreply.github.com>
2025-07-28 10:40:31 +0300

    Fixed incorrect scene type name.

--- Game/Source/Example/MyApplication.cpp
+++ Game/Source/Example/MyApplication.cpp
@@ -3,8 +3,8 @@
 
 Example::MyApplication::MyApplication() : Application(sf::Vector2u(800, 800), "Game works") {
     Blueprint::Game::SceneManager& sceneManager = getSceneManager();
-    const std::string sceneType = "Game::MyScene";
-    const std::filesystem::path scenePath = "Main Menu.bpscene";
+    const std::string sceneType = "Example::MyScene";
+    const std::filesystem::path scenePath = "Him.bpscene";
     sceneManager.registerScene<MyScene>(sceneType);
     sceneManager.loadScene(scenePath);
     sceneManager.setCurrentScene(scenePath);
